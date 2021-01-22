5c74194fa8a4f1b65535ae2989ee571613fd5947
luisCermeno <luis.cermeno.f@gmail.com>
2021-01-22 13:44:01 -0500

    Create Post model

--- network/models.py
+++ network/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username}"
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField("User", related_name="posts_liked", blank=True)
+
+    def __str__(self):
+        return f"Post by {self.user} on {self.timestamp}: {self.body}"
